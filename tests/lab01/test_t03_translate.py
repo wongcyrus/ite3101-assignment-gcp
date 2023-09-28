1f1a7ab688fb24260fe1bc2ae671c7a27238744f
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-09-28 15:58:42 +0800

    Update test_t03_translate.py

--- tests/lab01/test_t03_translate.py
+++ tests/lab01/test_t03_translate.py
@@ -27,8 +27,8 @@ class TestOutput(unittest.TestCase):
         self.assertEqual(expected_language_code, language_code)
 
     @parameterized.expand([
-        ("雲端系統及數據中心管理高級文憑課程編號 IT114115", "en", 'Higher Diploma in Cloud System and Data Center Management Course Code IT114115'),
-        ("Higher Diploma in Cloud and Data Centre Administration Programme code IT114115", "ja", "クラウドおよびデータ センター管理の高等ディプロマ プログラム コード IT114115"),
+        ("雲端系統及數據中心管理高級文憑課程編號 IT114115", "en", 'Higher Diploma in Cloud Systems and Data Center Management Course Number IT114115'),
+        ("Higher Diploma in Cloud and Data Centre Administration Programme code IT114115", "ja", "クラウドおよびデータセンター管理の高等ディプロマ プログラムコード IT114115"),
         ("ワンピース", "en", "one piece"),
     ])
     def test_02_translate_to(self, text: str, target_language_code: str, expected_translated_result: str):

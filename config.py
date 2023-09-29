4adfaedfc6fe054c25b3aedd4343ca629607e473
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-09-29 16:23:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 STUDENT_ID = "00000000000"
 API_KEY = ""
 
-API_ENDPOINT = 'https://api-pytest-runner-test-2223.azure-api.net'
+API_ENDPOINT = 'https://gateway-8eur6cwg.ue.gateway.dev'
 LOCATIONS = "asia-east2"
 PROJECT_ID = "ite3101-assignment"

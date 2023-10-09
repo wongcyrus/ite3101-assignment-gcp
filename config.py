2bd0a95fe495b6800e91250b076a5763e7fd92ac
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-10-09 14:28:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 STUDENT_ID = "979785543131" if os.environ.get(
     'STUDENT_ID') is None else os.environ.get('STUDENT_ID')
-API_KEY = "AIzaSyBzy23jbCFZ5IhHtxSShRl7GI-iRg2DVxU"
+API_KEY = ""
 
 API_ENDPOINT = 'https://gateway-8eur6cwg.ue.gateway.dev'
 LOCATIONS = "asia-east2"
